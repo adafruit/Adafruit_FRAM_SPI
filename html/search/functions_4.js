e5d563b6c4b4437d7fb2406feb7eca2b69071950
Doxygen CI <travis@travis-ci.org>
2020-02-09 18:48:58 +0000

    Deploy code docs to GitHub Pages Travis build:
    Commit:

--- html/search/functions_4.js
+++ html/search/functions_4.js
@@ -1,7 +1,5 @@
 var searchData=
 [
   ['setaddresssize',['setAddressSize',['../class_adafruit___f_r_a_m___s_p_i.html#a4943a931744adbe344714c21151f4ad9',1,'Adafruit_FRAM_SPI']]],
-  ['setstatusregister',['setStatusRegister',['../class_adafruit___f_r_a_m___s_p_i.html#a1dc7c37c9e8759f248be135d5c697cae',1,'Adafruit_FRAM_SPI']]],
-  ['spi_5ftransaction_5fend',['SPI_TRANSACTION_END',['../class_adafruit___f_r_a_m___s_p_i.html#a49bdf04054ea2ccb7ec8f0fa4c5971f3',1,'Adafruit_FRAM_SPI']]],
-  ['spi_5ftransaction_5fstart',['SPI_TRANSACTION_START',['../class_adafruit___f_r_a_m___s_p_i.html#a6818100aecab47ce0dd0b6589adfaf14',1,'Adafruit_FRAM_SPI']]]
+  ['setstatusregister',['setStatusRegister',['../class_adafruit___f_r_a_m___s_p_i.html#a1dc7c37c9e8759f248be135d5c697cae',1,'Adafruit_FRAM_SPI']]]
 ];
