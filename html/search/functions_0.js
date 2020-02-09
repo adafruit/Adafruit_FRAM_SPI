3cb736efab6f93312c544ac6c3ccfac59a4006e1
Doxygen CI <travis@travis-ci.org>
2020-02-09 18:08:03 +0000

    Deploy code docs to GitHub Pages Travis build:
    Commit:

--- html/search/functions_0.js
+++ html/search/functions_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['adafruit_5ffram_5fspi',['Adafruit_FRAM_SPI',['../class_adafruit___f_r_a_m___s_p_i.html#a5d038182970ee9da1a1cce52ec8655ce',1,'Adafruit_FRAM_SPI::Adafruit_FRAM_SPI(int8_t cs=-1, SPIClass *theSPI=&amp;SPI)'],['../class_adafruit___f_r_a_m___s_p_i.html#ac296d3084f661bdd4d5353f0f2cb8589',1,'Adafruit_FRAM_SPI::Adafruit_FRAM_SPI(int8_t clk, int8_t miso, int8_t mosi, int8_t cs)']]]
+  ['adafruit_5ffram_5fspi',['Adafruit_FRAM_SPI',['../class_adafruit___f_r_a_m___s_p_i.html#a271b1a3b7fda435661049a01873b13ce',1,'Adafruit_FRAM_SPI::Adafruit_FRAM_SPI(int8_t cs, SPIClass *theSPI=&amp;SPI)'],['../class_adafruit___f_r_a_m___s_p_i.html#ac296d3084f661bdd4d5353f0f2cb8589',1,'Adafruit_FRAM_SPI::Adafruit_FRAM_SPI(int8_t clk, int8_t miso, int8_t mosi, int8_t cs)']]]
 ];
