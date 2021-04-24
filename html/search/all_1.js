4e28fb08e318086f836ceb01f92aeb6550138ab4
Doxygen CI <ci-arduino@invalid>
2021-04-24 02:59:01 +0000

    Deploy docs to GitHub Pages from commit 5921d08d46
    Commit: 5921d08d4699f1576879e557c8bf571661216a7f
    GitHub Actions run: 779630312

--- html/search/all_1.js
+++ html/search/all_1.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['adafruit_5ffram_5fspi',['Adafruit_FRAM_SPI',['../class_adafruit___f_r_a_m___s_p_i.html',1,'Adafruit_FRAM_SPI'],['../class_adafruit___f_r_a_m___s_p_i.html#a271b1a3b7fda435661049a01873b13ce',1,'Adafruit_FRAM_SPI::Adafruit_FRAM_SPI(int8_t cs, SPIClass *theSPI=&amp;SPI)'],['../class_adafruit___f_r_a_m___s_p_i.html#ac296d3084f661bdd4d5353f0f2cb8589',1,'Adafruit_FRAM_SPI::Adafruit_FRAM_SPI(int8_t clk, int8_t miso, int8_t mosi, int8_t cs)']]],
+  ['adafruit_5ffram_5fspi',['Adafruit_FRAM_SPI',['../class_adafruit___f_r_a_m___s_p_i.html',1,'Adafruit_FRAM_SPI'],['../class_adafruit___f_r_a_m___s_p_i.html#a6f5c7fa36adeda7574873ee353dd49a2',1,'Adafruit_FRAM_SPI::Adafruit_FRAM_SPI(int8_t cs, SPIClass *theSPI=&amp;SPI, uint32_t freq=1000000)'],['../class_adafruit___f_r_a_m___s_p_i.html#ac296d3084f661bdd4d5353f0f2cb8589',1,'Adafruit_FRAM_SPI::Adafruit_FRAM_SPI(int8_t clk, int8_t miso, int8_t mosi, int8_t cs)']]],
   ['adafruit_5ffram_5fspi_2ecpp',['Adafruit_FRAM_SPI.cpp',['../_adafruit___f_r_a_m___s_p_i_8cpp.html',1,'']]],
   ['adafruit_5ffram_5fspi_2eh',['Adafruit_FRAM_SPI.h',['../_adafruit___f_r_a_m___s_p_i_8h.html',1,'']]],
   ['adafruit_20spi_20fram_20breakout_2e',['Adafruit SPI FRAM breakout.',['../index.html',1,'']]]
--- html/search/functions_0.js
+++ html/search/functions_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['adafruit_5ffram_5fspi',['Adafruit_FRAM_SPI',['../class_adafruit___f_r_a_m___s_p_i.html#a271b1a3b7fda435661049a01873b13ce',1,'Adafruit_FRAM_SPI::Adafruit_FRAM_SPI(int8_t cs, SPIClass *theSPI=&amp;SPI)'],['../class_adafruit___f_r_a_m___s_p_i.html#ac296d3084f661bdd4d5353f0f2cb8589',1,'Adafruit_FRAM_SPI::Adafruit_FRAM_SPI(int8_t clk, int8_t miso, int8_t mosi, int8_t cs)']]]
+  ['adafruit_5ffram_5fspi',['Adafruit_FRAM_SPI',['../class_adafruit___f_r_a_m___s_p_i.html#a6f5c7fa36adeda7574873ee353dd49a2',1,'Adafruit_FRAM_SPI::Adafruit_FRAM_SPI(int8_t cs, SPIClass *theSPI=&amp;SPI, uint32_t freq=1000000)'],['../class_adafruit___f_r_a_m___s_p_i.html#ac296d3084f661bdd4d5353f0f2cb8589',1,'Adafruit_FRAM_SPI::Adafruit_FRAM_SPI(int8_t clk, int8_t miso, int8_t mosi, int8_t cs)']]]
 ];
